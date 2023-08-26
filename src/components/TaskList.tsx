8bf6cd4810a7935454fa257f0ae69ac0d34ebe23
Iago-Santos-Sousa <iago.santos.sousa@gmail.com>
2023-08-26 19:58:40 -0300

    cm-20

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -4,24 +4,44 @@ import { BsPencil } from "react-icons/bs";
 import styles from "../assets/styles/Tolltip.module.css";
 import checkoboxIcon from "../assets/images/checkoboxIcon.svg";
 import unchecked from "../assets/images/unchecked.svg";
-import { useState } from "react";
-import { ITask } from "../interfaces/Task";
+import { useState, useEffect } from "react";
 
 const TaskList = ({
   taskList,
   handleDeleteTask,
   handleEditTask,
 }: TaskListType) => {
-  const [selectedTask, setSelectedTask] = useState<ITask[]>([]);
+  const [selectedTask, setSelectedTasks] = useState<number[]>([]);
   console.log(selectedTask);
 
-  const toogleCheckboxIcon = (task: ITask): void => {
-    // toogle de marcar checkbox ícone
-    if (selectedTask.includes(task)) {
-      setSelectedTask(selectedTask.filter((selected) => selected !== task));
-    } else {
-      setSelectedTask([...selectedTask, task]);
-    }
+  /*
+    **Evitar erro de tipagem do localStorage no useEffect abaixo:
+    1.Primeiro, armazenamos o resultado do localStorage.getItem("selectedTask") em savedSelectedItemsString, que é explicitamente do tipo string | null.
+
+    2.Em seguida, usamos um operador ternário para verificar se savedSelectedItemsString é nulo. Se não for nulo, fazemos o JSON.parse, caso contrário, atribuímos um array vazio.
+
+    3.Por fim, atribuímos savedSelectedItems a setSelectedTasks, garantindo que o tipo seja compatível.
+    
+    Isso deve resolver o erro e lidar com o possível valor nulo retornado pelo localStorage.
+    */
+  useEffect(() => {
+    const savedSelectedItemsString: string | null =
+      localStorage.getItem("selectedTask");
+    const savedSelectedItems: number[] = savedSelectedItemsString
+      ? JSON.parse(savedSelectedItemsString)
+      : [];
+    console.log(savedSelectedItems);
+    setSelectedTasks(savedSelectedItems);
+  }, []);
+
+  const toggleSelectedTask = (taskId: number): void => {
+    // Alternar checkbox da tarefa e incluir no localStorage
+    const updatedSelectedItems: number[] = selectedTask.includes(taskId)
+      ? selectedTask.filter((id) => id !== taskId)
+      : [...selectedTask, taskId];
+
+    setSelectedTasks(updatedSelectedItems);
+    localStorage.setItem("selectedTask", JSON.stringify(updatedSelectedItems));
   };
 
   return (
@@ -30,12 +50,10 @@ const TaskList = ({
         ? taskList.map((task, index) => (
             <div
               key={index}
-              onClick={() => {
-                toogleCheckboxIcon(task);
-              }}
+              onClick={() => toggleSelectedTask(task.id)}
               className={`tasks-container`}
               style={{
-                textDecoration: selectedTask.includes(task)
+                textDecoration: selectedTask.includes(task.id)
                   ? "line-through"
                   : "none",
               }}
@@ -60,7 +78,7 @@ const TaskList = ({
               <span className="checkbox">
                 <img
                   src={`${
-                    selectedTask.includes(task) ? checkoboxIcon : unchecked
+                    selectedTask.includes(task.id) ? checkoboxIcon : unchecked
                   }`}
                   alt="checkbox-icon"
                 />
